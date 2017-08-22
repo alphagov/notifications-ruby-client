1971efa00a75f2911a873a6acd051f8927ba9acd
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2017-08-22 15:59:03 +0100

    Add integration tests for sending letters

--- bin/test_client.rb
+++ bin/test_client.rb
@@ -15,8 +15,10 @@ def main
   test_generate_template_preview(client, ENV['EMAIL_TEMPLATE_ID'])
   email_notification = test_send_email_endpoint(client)
   sms_notification = test_send_sms_endpoint(client)
+  letter_notification = test_send_letter_endpoint(client)
   test_get_notification_by_id_endpoint(client, email_notification.id, 'email')
   test_get_notification_by_id_endpoint(client, sms_notification.id, 'sms')
+  test_get_notification_by_id_endpoint(client, letter_notification.id, 'letter')
   test_get_all_notifications(client)
   p 'ruby client integration tests pass'
   exit 0
@@ -34,16 +36,17 @@ end
 
 def test_get_all_templates(client)
   response = client.get_all_templates()
-  unless response.is_a?(Notifications::Client::TemplateCollection) then
+  unless response.is_a?(Notifications::Client::TemplateCollection)
     p 'failed test_get_all_templates response is not a Notifications::Client::TemplateCollection'
     exit 1
   end
-  unless response.collection.length >= 2 then
-    p 'failed test_get_all_templates, expected at least 2 templates returned.'
+  unless response.collection.length >= 3
+    p 'failed test_get_all_templates, expected at least 3 templates returned.'
     exit 1
   end
   test_template_response(response.collection[0], 'test_get_all_templates')
   test_template_response(response.collection[1], 'test_get_all_templates')
+  test_template_response(response.collection[2], 'test_get_all_templates')
 end
 
 def test_get_all_templates_filter_by_type(client)
@@ -105,46 +108,63 @@ def test_send_sms_endpoint(client)
   sms_resp
 end
 
+def test_send_letter_endpoint(client)
+  letter_resp = client.send_letter(
+    template_id: ENV['LETTER_TEMPLATE_ID'],
+    personalisation: {
+      address_line_1: "Her Majesty The Queen",
+      address_line_2: "Buckingham Palace",
+      postcode: "SW1 1AA"
+    },
+    reference: "some reference"
+  )
+  test_notification_response_data_type(letter_resp, 'letter')
+  letter_resp
+end
+
 def test_notification_response_data_type(notification, message_type)
-  unless notification.is_a?(Notifications::Client::ResponseNotification) then
+  unless notification.is_a?(Notifications::Client::ResponseNotification)
     p 'failed ' + message_type +' response is not a Notifications::Client::ResponseNotification'
     exit 1
   end
-  unless notification.id.is_a?(String) then
+  unless notification.id.is_a?(String)
     p 'failed '+ message_type + 'id is not a String'
     exit 1
   end
   field_should_not_be_nil(expected_fields_in_notification_response, notification, 'send_'+message_type)
   hash_key_should_not_be_nil(expected_fields_in_template, notification.send('template'), 'send_'+message_type+'.template')
-  if message_type == 'email' then
-    hash_key_should_not_be_nil(expected_fields_in_email_content, notification.send('content'), 'send_'+message_type+'.content')
-  end
-  if message_type == 'sms' then
-    hash_key_should_not_be_nil(expected_fields_in_sms_content, notification.send('content'),'send_'+message_type+'.content')
-  end
 
+  if message_type == 'email'
+    hash_key_should_not_be_nil(expected_fields_in_email_content, notification.send('content'), 'send_'+message_type+'.content')
+  elsif message_type == 'sms'
+    hash_key_should_not_be_nil(expected_fields_in_sms_content, notification.send('content'),'send_'+message_type+'.content')
+  elsif message_type == 'letter'
+    hash_key_should_not_be_nil(expected_fields_in_letter_content, notification.send('content'),'send_'+message_type+'.content')
+  end
 end
 
 def test_get_notification_by_id_endpoint(client, id, message_type)
   get_notification_response = client.get_notification(id)
 
-  unless get_notification_response.is_a?(Notifications::Client::Notification) then
+  unless get_notification_response.is_a?(Notifications::Client::Notification)
     p 'get notification is not a Notifications::Client::Notification for id ' + id
     exit 1
   end
-  if message_type == 'email' then
+
+  if message_type == 'email'
     field_should_not_be_nil(expected_fields_in_email_notification, get_notification_response, 'Notifications::Client::Notification for type email')
     field_should_be_nil(expected_fields_in_email_notification_that_are_nil, get_notification_response, 'Notifications::Client::Notification for type email')
     hash_key_should_not_be_nil(expected_fields_in_template, get_notification_response.send('template'), 'Notifications::Client::Notification.template for type email')
-  end
-  if message_type == 'sms' then
+  elsif message_type == 'sms'
     field_should_not_be_nil(expected_fields_in_sms_notification, get_notification_response, 'Notifications::Client::Notification for type sms')
     field_should_be_nil(expected_fields_in_sms_notification_that_are_nil, get_notification_response, 'Notifications::Client::Notification for type sms')
     hash_key_should_not_be_nil(expected_fields_in_template, get_notification_response.send('template'), 'Notifications::Client::Notification.template for type sms')
+  elsif message_type == 'letter'
+    field_should_not_be_nil(expected_fields_in_letter_notification, get_notification_response, 'Notifications::Client::Notification for type letter')
+    field_should_be_nil(expected_fields_in_letter_notification_that_are_nil, get_notification_response, 'Notifications::Client::Notification for type letter')
+    hash_key_should_not_be_nil(expected_fields_in_template, get_notification_response.send('template'), 'Notifications::Client::Notification.template for type letter')
   end
-
 end
-
 
 def hash_key_should_not_be_nil(fields, obj, method_name)
   fields.each do |field|
@@ -212,6 +232,13 @@ def expected_fields_in_sms_content
      )
 end
 
+def expected_fields_in_letter_content
+  %w(
+    body
+    subject
+  )
+end
+
 def expected_fields_in_email_notification
   %w(id
      email_address
@@ -259,6 +286,33 @@ def expected_fields_in_sms_notification_that_are_nil
      line_6
      postcode
      subject)
+end
+
+def expected_fields_in_letter_notification
+  %w(
+    id
+    type
+    status
+    template
+    body
+    subject
+    line_1
+    line_2
+    postcode
+    created_at
+  )
+end
+
+def expected_fields_in_letter_notification_that_are_nil
+  %w(
+    phone_number
+    email_address
+    line_3
+    line_4
+    line_5
+    line_5
+    line_6
+  )
 end
 
 def expected_fields_in_template
