ca9d548f14beae0d2dc2bbc38bb725484007baf8
Chris Hill-Scott <me@quis.cc>
2017-10-27 15:01:59 +0100

    Add a pull request template
    We should clarify what our expectations are of external developers
    contributing to our codebase.
    
    A good way to do this is through a pull request template, which lets
    developers check off what they’ve done. A lot of the GOV.UK frontend
    repos do this, eg
    https://github.com/alphagov/govuk_elements/blob/3c5c43a6fd4209cf2b6d180e7a5eadf6fa94c132/.github/PULL_REQUEST_TEMPLATE.md
    
    This commit also documents how we do version numbering, so that an
    external developer would have a reasonable chance of being able to
    increment the version number in the right way.

--- lib/notifications/client/version.rb
+++ lib/notifications/client/version.rb
@@ -1,3 +1,12 @@
+# Version numbering follows Semantic Versionning:
+#
+# Given a version number MAJOR.MINOR.PATCH, increment the:
+# - MAJOR version when you make incompatible API changes,
+# - MINOR version when you add functionality in a backwards-compatible manner, and
+# - PATCH version when you make backwards-compatible bug fixes.
+#
+# -- http://semver.org/
+
 module Notifications
   class Client
     VERSION = "2.3.0".freeze
