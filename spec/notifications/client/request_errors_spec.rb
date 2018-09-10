7b0468d0d77ec499e909cd71201034a1d0bf8260
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2018-09-10 14:22:09 +0100

    Handle server errors from the api
    We already had code to check if the api returns a client error and to
    raise a `RequestError` but we were not checking for or handling server
    errors. This meant that if someone tries to send a notification and
    there is a server error, instead of seeing that error the response from
    the client is a Notification object with `nil` for all its properties.
    
    This commit checks is the response from the api is a
    `Net::HTTPServerError` and raises a `RequestError` if it is.

--- spec/notifications/client/request_errors_spec.rb
+++ spec/notifications/client/request_errors_spec.rb
@@ -1,13 +1,10 @@
 describe Notifications::Client do
   let(:client) { build :notifications_client }
+  let(:uri) { URI.parse(Notifications::Client::PRODUCTION_BASE_URL) }
 
   describe "authorisation error" do
     let(:error_response) {
       attributes_for(:client_request_error)[:body]
-    }
-
-    let(:uri) {
-      URI.parse(Notifications::Client::PRODUCTION_BASE_URL)
     }
 
     before do
@@ -21,6 +18,25 @@ describe Notifications::Client do
     end
 
     it "raises exception" do
+      expect {
+        client.get_notification("1")
+      }.to raise_error(Notifications::Client::RequestError)
+    end
+  end
+
+  describe "server error" do
+    it "raises a Notifications::Client::RequestError" do
+      stub_request(
+        :get,
+        "https://#{uri.host}:#{uri.port}/v2/notifications/1"
+      ).to_return(
+        status: 503,
+        body: {
+                'status_code' => 503,
+                'errors' => ['error' => 'BadRequestError', 'message' => 'App error']
+              }.to_json
+      )
+
       expect {
         client.get_notification("1")
       }.to raise_error(Notifications::Client::RequestError)
